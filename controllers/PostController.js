24869502b1db7ef484f6f8e1876bf8f2e802a173
Pagonoudis <giorgospago23@gmail.com>
2022-05-11 20:37:16 +0300

    fix audio duration

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -260,7 +260,7 @@ const create = async (req, res) => {
     const videoInfo = await downloadVideoAndGetInfo(req.body.videoUrl, !!req.body.audio);
     const postData = {
         ...req.body,
-        duration: videoInfo.duration,
+        duration: videoInfo.duration || null,
         user: user._id
     };
     const post = new Post(postData);
