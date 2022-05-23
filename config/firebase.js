0cdd0a454b941baaeabfaa5b1843bb8fb9ee438c
Pagonoudis <giorgospago23@gmail.com>
2022-05-23 17:57:48 +0300

    fix google credentials

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,3 @@
-const {credential} = require('firebase-admin');
 const {initializeApp} = require('firebase-admin/app');
 
-global.fire = initializeApp({
-    credential: credential.cert(JSON.parse(process.env.SERVICE_ACCOUNT_STR))
-});
+global.fire = initializeApp();
